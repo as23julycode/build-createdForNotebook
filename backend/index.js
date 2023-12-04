1c71c217b80cbdd43021416d7e348b83b658483c
Aditya Singh <77926641+as23julycode@users.noreply.github.com>
2023-12-05 00:04:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express')
 var cors = require('cors')
-const path = require('path');
+// const path = require('path');
 
 connectToMongo();
 
@@ -16,10 +16,10 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
 // static file
-app.use(express.static(path.join(__dirname, '../build')));
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
-});
+// app.use(express.static(path.join(__dirname, '../build')));
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, '../build/index.html'));
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
